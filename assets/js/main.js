e522e392ac2bc0a9ffa9e307462e354792f58be4
prabin.thapamagar <prabin_mi@yonefu.info>
2024-10-22 05:55:29 +0545

    Navbar section design managed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,61 @@
 // JavaScript for Scroll Navbar Effect
-window.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".atel-header");
-  const scrollThreshold = 40;
+const navbar = document.querySelector(".atel-header");
+const scrollThreshold = 40;
 
+// Function to handle the scroll effect
+const handleScroll = () => {
   if (window.scrollY > scrollThreshold) {
     navbar.classList.add("atel-header-fixed");
   } else {
     navbar.classList.remove("atel-header-fixed");
   }
+};
+
+// Initial check on page load
+handleScroll();
+
+// Attach the scroll event listener
+window.addEventListener("scroll", handleScroll);
+
+// NAVBAR MENU TOGGLE
+const menuItems = document.querySelectorAll(".menu-item");
+let activeSubMenu = null;
+let activeMenuItem = null;
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const subMenuBlock = item.nextElementSibling;
+
+    if (subMenuBlock === activeSubMenu) {
+      subMenuBlock.classList.toggle("show");
+      item.classList.toggle("active");
+    } else {
+      resetNavbarMenu();
+      if (
+        subMenuBlock &&
+        subMenuBlock.classList.contains("submenu-item-block")
+      ) {
+        subMenuBlock.classList.add("show");
+        item.classList.add("active");
+        activeSubMenu = subMenuBlock;
+        activeMenuItem = item;
+      } else {
+        activeSubMenu = null;
+        activeMenuItem = null;
+      }
+    }
+  });
 });
+
+const resetNavbarMenu = () => {
+  document
+    .querySelectorAll(".submenu-item-block")
+    .forEach((subMenuBlock) => subMenuBlock.classList.remove("show"));
+
+  document
+    .querySelectorAll(".menu-item")
+    .forEach((menuItem) => menuItem.classList.remove("active"));
+};
 
 // WHO WE HELP SLIDER
 $(".who-we-help-slider").slick({
