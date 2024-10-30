12322961df83cadac4c2722020c11777298ec583
Sushovan bhattarai <dev13@esignature.com.np>
2024-10-30 17:52:29 +0545

    fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -264,36 +264,43 @@ document.querySelectorAll(".features-tabs li a").forEach((anchor) => {
 // });
 // window.addEventListener("resize", setEqualHeights);
 
-// WHO WE HELP SLIDER
-$(".who-we-help-slider, .sms-service-slider").slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  variableWidth: true,
-  arrows: true,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  prevArrow:
-    "<button type='button' class='slick-slider-btn slick-prev bg-white icon-52 border-grey-2-1 rounded-12'><img class='icon-24' src='./assets/icons/chevron_left_black.svg'></button>",
-  nextArrow:
-    "<button type='button' class='slick-slider-btn slick-next bg-white icon-52 border-grey-2-1 rounded-12'><img class='icon-24' src='./assets/icons/chevron_right_black.svg'></button>",
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        arrows: false,
-        dots: true,
+$(document).ready(function () {
+  // WHO WE HELP SLIDER
+  $(".who-we-help-slider, .sms-service-slider").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true,
+    arrows: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow:
+      "<button type='button' class='slick-slider-btn slick-prev bg-white icon-52 border-grey-2-1 rounded-12'><img class='icon-24' src='./assets/icons/chevron_left_black.svg'></button>",
+    nextArrow:
+      "<button type='button' class='slick-slider-btn slick-next bg-white icon-52 border-grey-2-1 rounded-12'><img class='icon-24' src='./assets/icons/chevron_right_black.svg'></button>",
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
       },
-    },
-    {
-      breakpoint: 576,
-      settings: {
-        variableWidth: false,
-        arrows: false,
-        dots: true,
+      {
+        breakpoint: 576,
+        settings: {
+          variableWidth: false,
+          arrows: false,
+          dots: true,
+        },
       },
-    },
-  ],
+    ],
+  });
+
+  AOS.init({
+    startEvent: "DOMContentLoaded",
+    duration: 600,
+  });
 });
